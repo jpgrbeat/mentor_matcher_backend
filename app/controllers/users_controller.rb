8aa51cd6118cd25c2d2a9a7ada440f483b61c46e
github username <github email address>
2018-09-07 09:42:07 -0400

    added User controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  before_action :find_user, only: [:show, :update, :destroy]
+
+  def index
+    render json: User.all
+  end
+
+  def create
+    render json: User.create(user_params)
+  end
+
+  def show
+    render json: @user
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  def destroy
+    render json: @user.destroy
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.permit(:name, :job_title, :type_of, :location, :bio, :email, :user)
+  end
+end
