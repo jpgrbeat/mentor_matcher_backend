3a83b991f94ffceb03fc7be79ad8fd8741e07cc9
Josh <joshua.ross.miller@gmail.com>
2018-09-07 09:36:00 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :users,:matches,:skills,:user_skills:
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
