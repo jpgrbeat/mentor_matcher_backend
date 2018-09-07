67a614d20d64d6a8b2d3e573dd6aac83525f1729
Josh <joshua.ross.miller@gmail.com>
2018-09-07 10:45:32 -0400

    update

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id,:name,:job_title,:type_of,:location,:bio,:email
   has_many :skills
-  has_many :matches 
+  has_many :user_skills
+  has_many :mentee_matches
+  has_many :mentor_matches
 end
