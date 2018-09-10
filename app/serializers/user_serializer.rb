20a9f1e77eb4690541bb4282233ac29f08f621b2
github username <github email address>
2018-09-10 14:22:29 -0400

    fixed bug in routes

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id,:name,:job_title,:type_of,:location,:bio,:email
-  has_many :user_skills
+  has_many :user_skills, only: [:number_of_years_experience]
   has_many :skills
   has_many :mentee_matches
   has_many :mentor_matches
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users,:matches,:skills,:user_skills
+  resources :users, :skills, :user_skills
+  resources :matches, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
