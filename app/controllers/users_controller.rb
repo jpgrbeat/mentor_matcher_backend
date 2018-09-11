6ce478f1fb5aa0be949e29b2098a84b060127559
github username <github email address>
2018-09-11 12:19:01 -0400

    took out find_user from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
 
-  before_action :find_user, only: [:show, :update, :destroy]
   skip_before_action :authenticate, only: [:index]
 
   def index
@@ -57,24 +56,7 @@ class UsersController < ApplicationController
       ])
   end
 
-  def update
-    @user.update(user_params)
-    if @user.save
-      render json: @user, status: :accepted
-    else
-      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
-    end
-  end
-
-  def destroy
-    render json: @user.destroy
-  end
-
   private
-
-  def find_user
-    @user = User.find(params[:id])
-  end
 
   def user_params
     params.require(:user).permit(:name, :job_title, :type_of, :location, :bio, :email,:password_digest)
